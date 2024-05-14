abc6503423386f47f02df35fab1890b4ee6e7f56
haikal0423 <mhaikal23azka@gmail.com>
2024-05-14 16:03:16 +0700

    Menambahkan destructor untuk kelas angka

---
+++ Destructor/Destructor.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+    class angka {
+    private:
+        int* arr;
+        int panjang;
+    public:
+        angka(int);     //construktor
+        ~angka();       //destructor
+        void cetakData();
+        void isiData();
+    };
+
+    angka::angka(int i) {
+        panjang = i;
+        arr = new int[i];
+        isiData();
+    }
+
+    angka::~angka() {    //destruktor
+        cout << endl;
+        cetakData();
+        delete[]arr;
+        cout << "Alamat Array Sudah Dilepaskan" << endl;
+    }
+
+    void angka::cetakData() {
+        for (int i = 1; i <= panjang; i++) {
+            cout << i << "=" << arr[i] << endl;
+        }
+    }
+
+    void angka::isiData() {
+        for (int i = 1; i <= panjang; i++) {
+            cout << i << " = ";
+            cin >> arr[i];
+        }
+        cout << endl;
+    }
+
+    int main() {
+        angka belajarcpp(3);                    //Construktor Dipanggil
+        angka* ptrBelajarcpp = new angka(5);    //Construktor Dipanggil
+        delete ptrBelajarcpp;                   //Destructor Dipanggil
+
+        return 0;
+    }//Destruktor Dipanggil
